ea78f1cccfc1c7005172eb5de323a088111ca5ad
laureanray <laureanray>
2018-02-10 00:43:42 +0800

    Trry

--- server/server.js
+++ server/server.js
@@ -2,11 +2,10 @@ const express = require('express');
 const path = require('path');
 let app = express();
 let publicPath = path.join(__dirname, '../public');
-let https_path = path.join(__dirname, '../.well-known');
+
 let port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
-app.use(express.static(https_path));
 
 
 
